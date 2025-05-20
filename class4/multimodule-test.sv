c3cff6fb8f95c77d1cff4f85214fb516a23b7179
Pdral <86361109+Pdral@users.noreply.github.com>
2025-05-20 14:49:07 -0300

    Create multimodule-test.sv

---
+++ class4/multimodule-test.sv
@@ -0,0 +1,49 @@
+module design_tb();
+  
+  logic a;
+  logic b;
+  logic c;
+  logic d;
+  logic s;
+  
+  design designgate(.*);
+  
+  initial begin
+    
+    $monitor(
+      $time,
+      "\nchaveA = ",
+      a,
+      "\nchaveB = ", 
+      b,
+      "\nchaveC = ", 
+      c,
+      "\nchaveD = ", 
+      d,
+      "\noutput = ",
+      s,
+      "\n"
+    );
+    
+    a = 0;
+    b = 0;
+    c = 0;
+    d = 0;
+    
+    #5
+    a = 1;
+    b = 1;
+    c = 1;
+    d = 1;
+    
+    #5
+    a = 1;
+    b = 0;
+    c = 0;
+    d = 1;
+    
+    #5
+    $display("End sim");
+  end
+  
+endmodule
